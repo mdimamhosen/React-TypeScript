abf92e3910c422ba30cd6b5bb6ce6d81941ed308
mdimamhosen <imamhosen9426224@gmail.com>
2024-09-12 19:21:34 +0600

    first commit

---
+++ src/assets/data.ts
@@ -0,0 +1,326 @@
+export const moviesData = [
+  {
+    id: 1,
+    title: "Inception",
+    thumbnail: {
+      trending: {
+        small:
+          "https://images.unsplash.com/photo-1658999167159-3f6659cace61?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        large:
+          "https://images.unsplash.com/photo-1611787640592-ebf78080d96e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+      regular: {
+        small:
+          "https://images.unsplash.com/photo-1514899706957-d22ee867a77b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        medium:
+          "https://images.unsplash.com/photo-1510827220565-c6a086ff31c8?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        large:
+          "https://images.unsplash.com/photo-1600660792241-240f01455c6f?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+    },
+    description:
+      "A skilled thief is given a chance to have his past crimes forgiven if he can successfully infiltrate a target's subconscious.",
+    year: 2010,
+    duration: "2h 28m",
+    rating: "PG-13",
+    isBookmarked: false,
+    isTrending: true,
+  },
+  {
+    id: 2,
+    title: "Interstellar",
+    thumbnail: {
+      trending: {
+        small:
+          "https://images.unsplash.com/photo-1658999167159-3f6659cace61?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        large:
+          "https://images.unsplash.com/photo-1611787640592-ebf78080d96e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+      regular: {
+        small:
+          "https://images.unsplash.com/photo-1514899706957-d22ee867a77b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        medium:
+          "https://images.unsplash.com/photo-1510827220565-c6a086ff31c8?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        large:
+          "https://images.unsplash.com/photo-1600660792241-240f01455c6f?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+    },
+    description:
+      "A group of astronauts travel through a wormhole in search of a new home for humanity.",
+    year: 2014,
+    duration: "2h 49m",
+    rating: "PG-13",
+    isBookmarked: true,
+    isTrending: true,
+  },
+  {
+    id: 3,
+    title: "The Dark Knight",
+    thumbnail: {
+      trending: {
+        small:
+          "https://images.unsplash.com/photo-1658999167159-3f6659cace61?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        large:
+          "https://images.unsplash.com/photo-1611787640592-ebf78080d96e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+      regular: {
+        small:
+          "https://images.unsplash.com/photo-1514899706957-d22ee867a77b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        medium:
+          "https://images.unsplash.com/photo-1510827220565-c6a086ff31c8?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        large:
+          "https://images.unsplash.com/photo-1600660792241-240f01455c6f?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+    },
+    description:
+      "Batman faces off against the Joker, a criminal mastermind who seeks to plunge Gotham City into anarchy.",
+    year: 2008,
+    duration: "2h 32m",
+    rating: "PG-13",
+    isBookmarked: true,
+    isTrending: false,
+  },
+  {
+    id: 4,
+    title: "The Matrix",
+    thumbnail: {
+      trending: {
+        small:
+          "https://images.unsplash.com/photo-1658999167159-3f6659cace61?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        large:
+          "https://images.unsplash.com/photo-1611787640592-ebf78080d96e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+      regular: {
+        small:
+          "https://images.unsplash.com/photo-1514899706957-d22ee867a77b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        medium:
+          "https://images.unsplash.com/photo-1510827220565-c6a086ff31c8?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        large:
+          "https://images.unsplash.com/photo-1600660792241-240f01455c6f?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+    },
+    description:
+      "A hacker discovers that the world he lives in is a simulated reality created by machines.",
+    year: 1999,
+    duration: "2h 16m",
+    rating: "R",
+    isBookmarked: false,
+    isTrending: false,
+  },
+  {
+    id: 5,
+    title: "Avatar",
+    thumbnail: {
+      trending: {
+        small:
+          "https://images.unsplash.com/photo-1658999167159-3f6659cace61?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        large:
+          "https://images.unsplash.com/photo-1611787640592-ebf78080d96e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+      regular: {
+        small:
+          "https://images.unsplash.com/photo-1514899706957-d22ee867a77b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        medium:
+          "https://images.unsplash.com/photo-1510827220565-c6a086ff31c8?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        large:
+          "https://images.unsplash.com/photo-1600660792241-240f01455c6f?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+    },
+    description:
+      "A paraplegic marine is dispatched to the moon Pandora on a unique mission, but he becomes torn between following orders and protecting the world he feels is his home.",
+    year: 2009,
+    duration: "2h 42m",
+    rating: "PG-13",
+    isBookmarked: true,
+    isTrending: true,
+  },
+  {
+    id: 6,
+    title: "Gladiator",
+    thumbnail: {
+      trending: {
+        small:
+          "https://images.unsplash.com/photo-1658999167159-3f6659cace61?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        large:
+          "https://images.unsplash.com/photo-1611787640592-ebf78080d96e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+      regular: {
+        small:
+          "https://images.unsplash.com/photo-1514899706957-d22ee867a77b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        medium:
+          "https://images.unsplash.com/photo-1510827220565-c6a086ff31c8?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        large:
+          "https://images.unsplash.com/photo-1600660792241-240f01455c6f?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+    },
+    description:
+      "A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.",
+    year: 2000,
+    duration: "2h 35m",
+    rating: "R",
+    isBookmarked: false,
+    isTrending: false,
+  },
+  {
+    id: 7,
+    title: "Parasite",
+    thumbnail: {
+      trending: {
+        small:
+          "https://images.unsplash.com/photo-1658999167159-3f6659cace61?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        large:
+          "https://images.unsplash.com/photo-1611787640592-ebf78080d96e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+      regular: {
+        small:
+          "https://images.unsplash.com/photo-1514899706957-d22ee867a77b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        medium:
+          "https://images.unsplash.com/photo-1510827220565-c6a086ff31c8?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        large:
+          "https://images.unsplash.com/photo-1600660792241-240f01455c6f?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+    },
+    description:
+      "Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.",
+    year: 2019,
+    duration: "2h 12m",
+    rating: "R",
+    isBookmarked: false,
+    isTrending: true,
+  },
+  {
+    id: 8,
+    title: "The Godfather",
+    thumbnail: {
+      trending: {
+        small:
+          "https://images.unsplash.com/photo-1658999167159-3f6659cace61?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        large:
+          "https://images.unsplash.com/photo-1611787640592-ebf78080d96e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+      regular: {
+        small:
+          "https://images.unsplash.com/photo-1514899706957-d22ee867a77b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        medium:
+          "https://images.unsplash.com/photo-1510827220565-c6a086ff31c8?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        large:
+          "https://images.unsplash.com/photo-1600660792241-240f01455c6f?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+    },
+    description:
+      "The aging patriarch of an organized crime dynasty transfers control of his empire to his reluctant son.",
+    year: 1972,
+    duration: "2h 55m",
+    rating: "R",
+    isBookmarked: true,
+    isTrending: false,
+  },
+  {
+    id: 9,
+    title: "Pulp Fiction",
+    thumbnail: {
+      trending: {
+        small:
+          "https://images.unsplash.com/photo-1658999167159-3f6659cace61?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        large:
+          "https://images.unsplash.com/photo-1611787640592-ebf78080d96e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+      regular: {
+        small:
+          "https://images.unsplash.com/photo-1514899706957-d22ee867a77b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        medium:
+          "https://images.unsplash.com/photo-1510827220565-c6a086ff31c8?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        large:
+          "https://images.unsplash.com/photo-1600660792241-240f01455c6f?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+    },
+    description:
+      "The lives of two mob , a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+    year: 1994,
+    duration: "2h 34m",
+    rating: "R",
+    isBookmarked: false,
+    isTrending: true,
+  },
+  {
+    id: 10,
+    title: "Fight Club",
+    thumbnail: {
+      trending: {
+        small:
+          "https://images.unsplash.com/photo-1658999167159-3f6659cace61?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        large:
+          "https://images.unsplash.com/photo-1611787640592-ebf78080d96e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+      regular: {
+        small:
+          "https://images.unsplash.com/photo-1514899706957-d22ee867a77b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        medium:
+          "https://images.unsplash.com/photo-1510827220565-c6a086ff31c8?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        large:
+          "https://images.unsplash.com/photo-1600660792241-240f01455c6f?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+    },
+    description:
+      "An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.",
+    year: 1999,
+    duration: "2h 19m",
+    rating: "R",
+    isBookmarked: false,
+    isTrending: false,
+  },
+  {
+    id: 11,
+    title: "Forrest",
+    thumbnail: {
+      trending: {
+        small:
+          "https://images.unsplash.com/photo-1658999167159-3f6659cace61?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        large:
+          "https://images.unsplash.com/photo-1611787640592-ebf78080d96e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+      regular: {
+        small:
+          "https://images.unsplash.com/photo-1514899706957-d22ee867a77b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        medium:
+          "https://images.unsplash.com/photo-1510827220565-c6a086ff31c8?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        large:
+          "https://images.unsplash.com/photo-1600660792241-240f01455c6f?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+    },
+    description:
+      "The presidencies of Kennedy and Johnson, the Vietnam War, the Watergate scandal, and other historical events unfold from the perspective of an Alabama man with an IQ of 75.",
+    year: 1994,
+    duration: "2h 22m",
+    rating: "PG-13",
+    isBookmarked: true,
+    isTrending: true,
+  },
+  {
+    id: 12,
+    title: "The  Redemption",
+    thumbnail: {
+      trending: {
+        small:
+          "https://images.unsplash.com/photo-1658999167159-3f6659cace61?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        large:
+          "https://images.unsplash.com/photo-1611787640592-ebf78080d96e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+      regular: {
+        small:
+          "https://images.unsplash.com/photo-1514899706957-d22ee867a77b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        medium:
+          "https://images.unsplash.com/photo-1510827220565-c6a086ff31c8?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        large:
+          "https://images.unsplash.com/photo-1600660792241-240f01455c6f?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+    },
+    description:
+      "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+    year: 1994,
+    duration: "2h 22m",
+    rating: "R",
+    isBookmarked: false,
+    isTrending: false,
+  },
+];
